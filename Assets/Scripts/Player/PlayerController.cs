2e5e0dda8e736457a42f2af43ca5af071b5ea260
Kimwoojong <wooj950713@naver.com>
2024-02-13 17:10:17 +0900

    [add] player Animation

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,8 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem.HID;
-using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
